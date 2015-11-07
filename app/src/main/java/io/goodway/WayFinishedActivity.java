3df2251540d9bdb4f11f47e5e79eb90b482c95ed
Antoine Sauray <sauray.a@outlook.com>
2015-11-07 17:34:25 +0100

    Qr Code generator

---
+++ app/src/main/java/io/goodway/WayFinishedActivity.java
@@ -0,0 +1,7 @@
+package io.goodway;
+
+/**
+ * Created by antoine on 11/7/15.
+ */
+public class WayFinishedActivity {
+}
---
+++ app/src/main/java/io/goodway/model/bonus/QrCode.java
@@ -0,0 +1,7 @@
+package io.goodway.model.bonus;
+
+/**
+ * Created by antoine on 11/7/15.
+ */
+public class QrCode {
+}
